4d75e9984f7935ffc13ff02fb50e4d06cdbe747e
Michael Ishola <myckhel1@hotmail.com>
2022-07-24 15:07:26 +0100

    Separated hooks route from the route group (#40)

--- config/paystack.php
+++ config/paystack.php
@@ -7,7 +7,8 @@ return [
   "merchant_email"    => env("PAYSTACK_MERCHANT_EMAIL"),
 
   "route" => [
-    "middleware"    => ['paystack_route_disabled', 'api'], // For injecting middleware to the package's routes
-    "prefix"        => 'api', // For injecting middleware to the package's routes
+    "middleware"        => ['paystack_route_disabled', 'api'], // For injecting middleware to the package's routes
+    "prefix"            => 'api', // For injecting middleware to the package's routes
+    'hook_middleware'   => ['paystack_route_disabled', 'api']
   ],
 ];
